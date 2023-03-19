d789a4c4f80a9d82a3c353205e0990a9a3cfd73a
xinran <wxinran6@vt.edu>
2023-03-19 23:54:40 +0000

    child c++ class and change parent class blueprint

---
+++ Source/ToonTanks/Tank.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tank.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+ATank::ATank()
+{
+    springArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+    springArm->SetupAttachment(RootComponent);
+
+    camera = CreateDefaultSubobject<UCameraComponent>(TEXT("CAMERA"));
+    camera->SetupAttachment(springArm);
+
+}
---
+++ Source/ToonTanks/Tank.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+	
+public:
+	ATank();
+
+private:
+
+	UPROPERTY(VisibleAnywhere)
+	class USpringArmComponent* springArm;
+
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent* camera;
+
+	 
+};
