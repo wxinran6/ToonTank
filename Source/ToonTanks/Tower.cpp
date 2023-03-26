adabce8b6293bb14fb295700296ca94c3448d533
xinran <wxinran6@vt.edu>
2023-03-26 23:22:17 +0100

    Tower check Tank and rotate

---
+++ Source/ToonTanks/Tower.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tower.h"
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
+
+void ATower::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+
+    if (Distance <= FireRange)
+    {
+        RotateTurrent(Tank->GetActorLocation());
+    }
+    
+}
+
+void ATower::BeginPlay()
+{
+    Super::BeginPlay();
+
+    Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+
+
+}
